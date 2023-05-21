29f6857381b458264078124099c0b3245d423b08
Indiagov <134226695+Indiagov@users.noreply.github.com>
2023-05-22 02:38:06 +0530

    Delete New folder directory

--- New folder/script.js
+++
@@ -1,3 +0,0 @@
-// Generate a random total marks between 271 and 500
-var totalMarks = Math.floor(Math.random() * (500 - 271 + 1)) + 271;
-document.getElementById('totalMarks').textContent = totalMarks;
