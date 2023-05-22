94448f74576559ea683b96744a24bd02032ab326
Indiagov <134226695+Indiagov@users.noreply.github.com>
2023-05-22 11:08:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var totalMarks = localStorage.getItem('totalMarks');
 if (!totalMarks || totalMarks <= 380 || totalMarks > 480) {
   do {
     totalMarks = Math.floor(Math.random() * (480 - 381 + 1)) + 381;
-  } while (totalMarks <= 345 || totalMarks > 500);
+  } while (totalMarks <= 390 || totalMarks > 530);
   
   localStorage.setItem('totalMarks', totalMarks);
 }
