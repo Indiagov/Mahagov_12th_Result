baabb86d66a0f2dcd7f8437557c924ce1000e236
Indiagov <134226695+Indiagov@users.noreply.github.com>
2023-05-22 09:26:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 var totalMarks = localStorage.getItem('totalMarks');
 
-if (!totalMarks || totalMarks > 380) {
+if (!totalMarks || totalMarks <= 285 || totalMarks > 380) {
   do {
     totalMarks = Math.floor(Math.random() * (500 - 271 + 1)) + 271;
-  } while (totalMarks > 380);
+  } while (totalMarks <= 285 || totalMarks > 380);
   
   localStorage.setItem('totalMarks', totalMarks);
 }
 
-var formattedTotalMarks = totalMarks + "/100";
+var formattedTotalMarks = totalMarks + "/500";
 document.getElementById('totalMarks').textContent = formattedTotalMarks;
-
