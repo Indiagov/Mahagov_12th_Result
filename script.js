b1df6050c14277fb18d730b218eb18a153fc33e6
Indiagov <134226695+Indiagov@users.noreply.github.com>
2023-05-22 09:18:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-// Check if the random value is already stored in local storage
-var storedTotalMarks = localStorage.getItem('totalMarks');
+var totalMarks = localStorage.getItem('totalMarks');
 
-if (storedTotalMarks) {
-  // Use the stored value if it exists
-  var totalMarks = parseInt(storedTotalMarks);
-} else {
-  // Generate a new random value if it doesn't exist and store it
-  totalMarks = Math.floor(Math.random() * (500 - 271 + 1)) + 271;
+if (!totalMarks || totalMarks > 380) {
+  do {
+    totalMarks = Math.floor(Math.random() * (500 - 271 + 1)) + 271;
+  } while (totalMarks > 380);
+  
   localStorage.setItem('totalMarks', totalMarks);
 }
 
-var formattedTotalMarks = totalMarks + "/500";
+var formattedTotalMarks = totalMarks + "/100";
 document.getElementById('totalMarks').textContent = formattedTotalMarks;
+
